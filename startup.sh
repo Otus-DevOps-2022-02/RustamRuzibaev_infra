7ba7e0952ebec2392107f67072093b7a55b660bf
Rustam Ruzibaev <RustamRuzibaev.work@gmail.com>
2022-04-30 18:10:34 +0300

    Added metadata.yml

--- startup.sh
+++ startup.sh
@@ -4,6 +4,6 @@ yc compute instance create \
   --hostname reddit-app \
   --memory=4 \
   --create-boot-disk image-folder-id=standard-images,image-family=ubuntu-1604-lts,size=10GB \
-  --network-interface subnet-name=infra-ru-central1-a,nat-ip-version=ipv4,nat-address=51.250.86.251 \
+  --network-interface subnet-name=infra-ru-central1-a,nat-ip-version=ipv4,nat-address=51.250.86.251\
   --metadata serial-port-enable=1 \
   --metadata-from-file user-data=metadata.yaml

577aa442c5e9c6e38a370838ae90c43122283ff9
Rustam Ruzibaev <RustamRuzibaev.work@gmail.com>
2022-04-30 18:15:03 +0300

    Fixed metadata.yml

--- startup.sh
+++ startup.sh
@@ -6,4 +6,4 @@ yc compute instance create \
   --create-boot-disk image-folder-id=standard-images,image-family=ubuntu-1604-lts,size=10GB \
   --network-interface subnet-name=infra-ru-central1-a,nat-ip-version=ipv4,nat-address=51.250.86.251\
   --metadata serial-port-enable=1 \
-  --metadata-from-file user-data=metadata.yaml
+  --metadata-from-file user-data=metadata.yml

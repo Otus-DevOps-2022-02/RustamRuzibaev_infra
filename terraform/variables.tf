4f6958f6a67234cdd811d67b40f725cdd6f93b75
Rustam Ruzibaev <RustamRuzibaev.work@gmail.com>
2022-07-28 20:05:36 +0300

    Homework terraform-1 DONE

---
+++ terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "token_id" {}
+variable "cloud_id" {}
+variable "folder_id" {}
+variable "yc_instance_zone" { default = "ru-central1-a" }
+variable "public_key_path" {}
+variable "private_key_path" {}
+variable "image_id" {}
+variable "subnet_id" {}

c235bc40c08d23f3691e598259da888935673483
Rustam Ruzibaev <RustamRuzibaev.work@gmail.com>
2022-07-28 20:12:57 +0300

    fixed main.tf for tests

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    yandex = {
+#      source = "yandex-cloud/yandex"
+#    }
+#  }
+#}
 provider "yandex" {
   token     = var.token_id
   cloud_id  = var.cloud_id
